5cd6bd26ec96a0ebc0553eb12a04bd738ebc8968
hooqii <hooqii.me@gmail.com>
2024-02-29 09:45:12 +0700

    Update the code

---
+++ external-module/main.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const port = 8000;
+
+// Membuat server HTTP
+const server = http.createServer((req, res) => {
+    if (req.url === "/home") {
+        const data = fs.readFileSync('./home.html');
+        res.end(data);
+    } else if (req.url === "/alex") {
+        const data = fs.readFileSync('./index.html');
+        res.end(data)
+    } else {
+        res.end("Hello JavaScript");
+    }
+})
+
+// Mendengarkan permintaan pada kode tertentu
+server.listen(port, () => {
+    console.log(`Server berjalan di ${port}`)
+})
